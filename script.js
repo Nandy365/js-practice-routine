5d848e01436c217b5cf5d88c7e0daa885827c2e8
Nandy365 <kumarnan217@gmail.com>
2023-08-21 13:31:33 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// let firstname ="nandy"
+// let number = 1;
+// let student = "true"
+// number=number+21
+// console.log(firstname)
+// console.log(number)
+// console.log(student)
+
+// document.getElementById("p1").innerHTML="my name is"+firstname;
+// document.getElementById("p2").innerHTML="my age "+number;
+// document.getElementById("p3").innerHTML="i am self thought developer"+student;
+
+//arithmetic
+
+let student = 40;
+//student=student+1;
+//student=student-21;
+//student=student*2
+//let extra = student/2
+//let extra = student%3
+
+student +=20;
+console.log(student)
