5106106bad91eb9999f2684d9e489a6cfb47a48f
Nandy365 <kumarnan217@gmail.com>
2023-09-20 13:51:10 +0530

    sort an array of strings

--- script.js
+++ script.js
@@ -509,16 +509,27 @@
 
 //loop through an array
 
-let prices = [1,2,3,4,5];
+// let prices = [1,2,3,4,5];
 
-for(let i = prices.length -1; i >= 0; i-=1){
-    console.log(prices[i])
+// for(let i = prices.length -1; i >= 0; i-=1){
+//     console.log(prices[i])
 
-}
+// }
 
 
-let pricess = [1,2,3,4,5]
+// let pricess = [1,2,3,4,5]
 
-for (i = 0; i < pricess.length; i+=1){
-    console.log(pricess[i])
+// for (i = 0; i < pricess.length; i+=1){
+//     console.log(pricess[i])
+// }
+
+//sort an array of strings
+
+let cars = ["audi","bmw","benze","tata","rangerover"]
+
+cars = cars.sort()
+cars = cars.sort().reverse()
+
+for(let car of cars){
+    console.log(car)
 }
