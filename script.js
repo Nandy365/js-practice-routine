451049c52e624fb7a582dded55397efb0f18d212
Nandy365 <kumarnan217@gmail.com>
2023-10-10 20:52:30 +0530

    detect presses

--- script.js
+++ script.js
@@ -1540,16 +1540,48 @@
 //     this.style.backgroundColor="red"
 // }
 
-//show/hidden HTML elements
+//show/hide HTML elements
 
-const mybutton = document.querySelector("#mybutton")
-const lambo = document.querySelector("#lambo")
+// const mybutton = document.querySelector("#mybutton")
+// const lambo = document.querySelector("#lambo")
 
-mybutton.addEventListener("click", () => {
-    if(lambo.style.visibility == "hidden"){
-        lambo.style.visibility = "visible"
+// mybutton.addEventListener("click", () => {
+//     if(lambo.style.visibility == "hidden"){
+//         lambo.style.visibility = "visible"
+//     }
+//     else{
+//         lambo.style.visibility = "hidden"
+//     }
+// })
+
+
+//detect presses
+
+const myDiv = document.getElementById("myDiv")
+
+window.addEventListener("keydown",move)
+let y = 0
+let x = 0
+
+function move(event){
+    switch(event.key){
+        case "ArrowDown":
+        y+=5
+        myDiv.style.top = y + "px"
+        break;
+        case "ArrowUp":
+        y-=5;
+        myDiv.style.top = y + "px"
+        break;
+        case "ArrowLeft":
+        x-=5
+        myDiv.style.left = x + "px"
+        break;
+        case "ArrowRight":
+        x+=5
+        myDiv.style.left = x + "px"
+        break;
+        default:
+            break;
     }
-    else{
-        lambo.style.visibility = "hidden"
-    }
-})
+}
