499aa1dd46cedc33d4cd2b076c20c8907c787ad3
Nandy365 <kumarnan217@gmail.com>
2023-10-18 23:07:12 +0530

    stage 1

--- script.js
+++ script.js
@@ -1814,61 +1814,68 @@
 
 //stopwatch
 
-const timeDisplay = document.querySelector("#timeDisplay")
-const startBtn = document.querySelector("#startBtn")
-const pauseBtn = document.querySelector("#pauseBtn")
-const resetBtn = document.querySelector("#resetBtn")
+// const timeDisplay = document.querySelector("#timeDisplay")
+// const startBtn = document.querySelector("#startBtn")
+// const pauseBtn = document.querySelector("#pauseBtn")
+// const resetBtn = document.querySelector("#resetBtn")
 
-let startTime = 0;
-let elapsedTime = 0;
-let currentTime = 0;
-let paused = true;
-let intervalId;
-let hrs = 0;
-let mins = 0;
-let secs = 0;
-let mill = 0;
-startBtn.addEventListener("click", () => {
-    if(paused){
-        paused = false;
-        startTime = Date.now() - elapsedTime;
-        intervalId = setInterval(updateTime, 75)
-    }
-});
-pauseBtn.addEventListener("click",() =>{
-    if(!paused){
-        paused = true;
-        elapsedTime = Date.now() - startTime;
-        clearInterval(intervalId)
-    }
-});
-resetBtn.addEventListener("click",() =>{
-    paused = true;
-    clearInterval(intervalId)
-    startTime = 0;
-    elapsedTime = 0;
-    currentTime = 0;
-    hrs = 0;
-    mins = 0;
-    secs = 0;
-    mill = 0;
-    timeDisplay.textContent ="00:00:00:00"
+// let startTime = 0;
+// let elapsedTime = 0;
+// let currentTime = 0;
+// let paused = true;
+// let intervalId;
+// let hrs = 0;
+// let mins = 0;
+// let secs = 0;
+// let mill = 0;
+// startBtn.addEventListener("click", () => {
+//     if(paused){
+//         paused = false;
+//         startTime = Date.now() - elapsedTime;
+//         intervalId = setInterval(updateTime, 75)
+//     }
+// });
+// pauseBtn.addEventListener("click",() =>{
+//     if(!paused){
+//         paused = true;
+//         elapsedTime = Date.now() - startTime;
+//         clearInterval(intervalId)
+//     }
+// });
+// resetBtn.addEventListener("click",() =>{
+//     paused = true;
+//     clearInterval(intervalId)
+//     startTime = 0;
+//     elapsedTime = 0;
+//     currentTime = 0;
+//     hrs = 0;
+//     mins = 0;
+//     secs = 0;
+//     mill = 0;
+//     timeDisplay.textContent ="00:00:00:00"
 
-});
+// });
 
-function updateTime(){
-    elapsedTime = Date.now() - startTime;
-    mill = Math.floor((elapsedTime % 1000) / 100)
-    secs = Math.floor((elapsedTime / 1000) % 60)
-    mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
-    hrs = Math.floor((elapsedTime /  (1000 * 60 * 60)) % 60)
+// function updateTime(){
+//     elapsedTime = Date.now() - startTime;
+//     mill = Math.floor((elapsedTime % 1000) / 100)
+//     secs = Math.floor((elapsedTime / 1000) % 60)
+//     mins = Math.floor((elapsedTime / (1000 * 60)) % 60);
+//     hrs = Math.floor((elapsedTime /  (1000 * 60 * 60)) % 60)
 
-    secs = pad(secs);
-    mins = pad(mins);
-    hrs = pad(hrs);
-    mill = pad(mill);
-    timeDisplay.textContent = `${hrs}:${mins}:${secs}:${mill}`;
-    function pad(unit){
-        return (("0") + unit).length > 2 ? unit : "0" + unit;
-    }
-}
+//     secs = pad(secs);
+//     mins = pad(mins);
+//     hrs = pad(hrs);
+//     mill = pad(mill);
+//     timeDisplay.textContent = `${hrs}:${mins}:${secs}:${mill}`;
+//     function pad(unit){
+//         return (("0") + unit).length > 2 ? unit : "0" + unit;
+//     }
+// }
+
+
+
+const playerText = document.querySelector("#playerText")
+const computerText = document.querySelector("#computerText")
+const resultText = document.querySelector("#resultText")
+
