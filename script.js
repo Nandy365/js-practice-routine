038002ef030b188514c022f84e89b4200c7a3133
Nandy365 <kumarnan217@gmail.com>
2023-10-25 14:28:41 +0530

    rock paper scissor program

--- script.js
+++ script.js
@@ -1878,4 +1878,50 @@
 const playerText = document.querySelector("#playerText")
 const computerText = document.querySelector("#computerText")
 const resultText = document.querySelector("#resultText")
+const choiceBtns = document.querySelectorAll(".choiceBtn")
 
+let player;
+let computer;
+let result;
+
+
+choiceBtns.forEach(button => button.addEventListener("click" ,() =>{
+
+    player = button.textContent;
+    cumputerTurn();
+    playerText.textContent = `Player: ${player}`
+    computerText.textContent = `Computer: ${computer}`
+    resultText.textContent = checkWinner();
+}));
+
+function cumputerTurn(){
+    const randNum = Math.floor(Math.random() *3) + 1;
+
+    switch(randNum){
+        case 1:
+        computer = "ROCK";
+        break;
+        case 2:
+        computer = "PAPER";
+        break;
+        case 3:
+        computer = "SCISSORS";
+        break;
+    }
+
+}
+
+function checkWinner(){
+    if(player == computer){
+        return "Draw"
+    }
+    else if(computer == "ROCK"){
+        return (player == "PAPER") ? "you Win" : "you Lose"
+    }
+    else if(computer == "PAPER"){
+        return (player == "SCISSORS") ? "you Win" : "you Lose"
+    }
+    else if(computer == "SCISSORS"){
+        return (player == "ROCK") ? "you Win" : "you Lose"
+    }
+}
